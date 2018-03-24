f6d35407869af5ce6144b5ef94be4c7d1ff2a0dc
francisco.bisquerra <fbisquerra@hiberus.com>
2018-03-24 20:14:59 +0100

    initial commit

---
+++ src/main/java/com/myvirtualshowcase/virtualshowcasewebapp/VirtualshowcasewebappApplication.java
@@ -0,0 +1,12 @@
+package com.myvirtualshowcase.virtualshowcasewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualshowcasewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualshowcasewebappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myvirtualshowcase/virtualshowcasewebapp/VirtualshowcasewebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myvirtualshowcase.virtualshowcasewebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualshowcasewebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
